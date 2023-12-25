0a45105d549c6f0ac79ec85f1e676d9040773b33
MD ABDULLAH <mdabdullah161036@gmail.com>
2023-12-25 17:16:35 +0600

    calculate done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const calculateExpense = () => {
-  const income = document.querySelector("income").value;
-  const food = document.querySelector("food").value;
-  const rent = document.querySelector("rent").value;
-  const clothes = document.querySelectorAll("clothes").value;
+  const income = document.querySelector("#income").value;
+  const food = document.querySelector("#food").value;
+  const rent = document.querySelector("#rent").value;
+  const clothes = document.querySelector("#clothes").value;
 
   if (
     income < 0 ||
@@ -18,13 +18,13 @@ const calculateExpense = () => {
     return;
   }
   // calculate expense
-  const expense =
-    parseInt(food) + parseInt(rent) + parseInt(clothes);
+  const expense = parseInt(food) + parseInt(rent) + parseInt(clothes);
 
   // calculate balance
-  const balance = parseInt(income.value) - expense;
+  // income theke expense minus korle total balance pawa jabe
+  const balance = parseInt(income) - expense;
   //   validate income
-  if (expense > income.value) {
+  if (expense > income) {
     alert("Expenses cannot be more than income");
   } else {
     // view total expense and balance
@@ -36,15 +36,14 @@ const calculateExpense = () => {
 const calculateSavings = () => {
   // calculate saving amount
   const savePercentage = document.getElementById("save").value;
-//   Validate saving percentage value
+  //   Validate saving percentage value
   if (savePercentage < 0) {
     alert("Provide positive saving value");
   }
-  const savingAmount = (savePercentage / 100) + income;
+  const savingAmount = savePercentage / 100 + income;
 
   // calculate remaining balance
   const balance = document.getElementById("balance").innerText;
-  const remainingBalance = balance - savingAmount;
 
   //   validate saving amount
   if (savingAmount > balance) {
